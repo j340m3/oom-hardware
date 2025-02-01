aa35fdbdaf2dbc5b4caf3aa9e5c4e2d8c9879280
robert jakub <rj@project2.pl>
2025-02-01 10:52:06 +0100

    fix flake imports

--- flake.nix
+++ flake.nix
@@ -1,20 +1,10 @@
 {
   description = "oom-hardware";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-  outputs = {
-    nixpkgs,
-    nixos-hardware,
-    ...
-  }: let
-    system = "aarch64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
+  outputs = {...}: {
     nixosModules = {
-      uconsole = import ./uconsole {inherit pkgs nixos-hardware;};
-      deskpi = import ./deskpi {inherit pkgs nixos-hardware;};
+      uconsole = import ./uconsole;
+      deskpi = import ./deskpi;
       cm4 = import ./cm4;
     };
   };
