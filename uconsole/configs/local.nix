2a8e01c2041336dca4c09ab2bbb281778ccf042f
robert jakub <rj@project2.pl>
2025-02-02 21:53:57 +0100

    move binary cache to 'newbox'

--- uconsole/configs/local.nix
+++ uconsole/configs/local.nix
@@ -9,7 +9,7 @@ in {
   nix.settings = {
     substituters = ["https://cache-nix.project2.xyz/uconsole"];
     trusted-substituters = ["https://cache-nix.project2.xyz/uconsole"];
-    trusted-public-keys = ["uconsole:t2pv3NWEtXCYY0fgv9BB8r9tRdK+Tz7HYhGq9bXIIck="];
+    trusted-public-keys = ["uconsole:vvqOLjqEwTJBUqv1xdndD1YHcdlMc/AnfAz4V9Hdxyk="];
     experimental-features = ["nix-command" "flakes"];
   };
   services.openssh.enable = true;
